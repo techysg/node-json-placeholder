010d4f7de25ca85f0c4058e5550433d592c4191a
Shubham Gupta <shubham@99fusion.com>
2021-11-02 13:58:50 +0530

    index.js to app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/photos', require('./routes/photos'));
 
 //How to listen to server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,function(err){
+app.listen(PORT, function (err) {
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", PORT);
 })
